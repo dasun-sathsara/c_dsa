e81ce45e25d8d568c9112b56ae98552eddefde2f
Dasun <x@dasunsathsara.com>
2024-02-04 09:39:40 +0530

    Add unit tests for ArrayList functions

--- tests/test_arraylist.c
+++ tests/test_arraylist.c
@@ -1,50 +1,79 @@
 #include "../src/arraylist.h"
 #include <assert.h>
+#include <criterion/criterion.h>
+#include <stdbool.h>
 #include <stdio.h>
 
-void try_out()
+Test(arraylist, test_arraylist_create, .description = "Test creating a new ArrayList")
 {
-    ArrayList *arr;
-    arr = arraylist_new(10);
-    printf("The capacity of the list is: %zu\n", arr->capacity);
-
-    // Add elements to the list
-    int a = 10;
-    int b = 22;
-
-    arraylist_append(arr, &a);
-    arraylist_append(arr, &b);
-
-    for (int i = 0; i < 10; i++)
-    {
-        int *t = malloc(sizeof(int));
-        *t = i;
-        arraylist_append(arr, t);
-    }
-
-    for (size_t i = 0; i < arr->size; i++)
-    {
-        printf("The element at index %zu is: %d\n", i, *(int *)arr->items[i]);
-    }
-
-    // Free dynamically allocated memory
-    for (size_t i = 2; i < arr->size; i++)
-    {
-        free(arr->items[i]);
-    }
-
-    arraylist_free(arr);
+    ArrayList *list = arraylist_new(10);
+    cr_assert_not_null(list);
+    cr_assert_eq(list->size, 0);
+    cr_assert_eq(list->capacity, 10);
+    arraylist_free(list);
 }
 
-int main()
+Test(arraylist, test_arraylist_append, .description = "Test appending items to an ArrayList")
 {
-    printf("Testing ArrayList\n");
-    try_out();
     ArrayList *list = arraylist_new(10);
-    assert(list != NULL);
-    assert(list->size == 0);
-    assert(list->capacity == 11);
-    assert(list->items != NULL);
+    cr_assert_not_null(list);
+    cr_assert_eq(list->size, 0);
+    cr_assert_eq(list->capacity, 10);
 
-    return 0;
+    int a = 34;
+    int b = 56;
+
+    arraylist_append(list, &a);
+    cr_assert_eq(list->size, 1);
+    cr_assert_eq(list->capacity, 10);
+
+    arraylist_append(list, &b);
+    cr_assert_eq(list->size, 2);
+    cr_assert_eq(list->capacity, 10);
+
+    cr_assert_eq(*(int *)list->items[0], a);
+
+    arraylist_free(list);
+}
+
+Test(arraylist, test_arraylist_remove, .description = "Test removing items from an ArrayList")
+{
+    ArrayList *list = arraylist_new(10);
+
+    int a = 14;
+    int b = 556;
+
+    arraylist_append(list, &a);
+    arraylist_append(list, &b);
+
+    arraylist_remove(list, 0);
+    cr_assert_eq(list->size, 1);
+    cr_assert_eq(list->capacity, 10);
+    cr_assert_eq(*(int *)list->items[0], b);
+
+    arraylist_free(list);
+}
+
+bool cb(ArrayListItem item1, ArrayListItem item2)
+{
+    int a = *(int *)item1;
+    int b = *(int *)item2;
+
+    return a == b ? true : false;
+}
+
+Test(arraylist, test_arraylist_index_of, .description = "Test finding the index of an item in an ArrayList")
+{
+    ArrayList *list = arraylist_new(10);
+
+    int a = 654;
+    int b = 536;
+
+    arraylist_append(list, &a);
+    arraylist_append(list, &b);
+
+    cr_assert_eq(arraylist_index_of(list, cb, &a), 0);
+    cr_assert_eq(arraylist_index_of(list, cb, &b), 1);
+
+    arraylist_free(list);
 }
