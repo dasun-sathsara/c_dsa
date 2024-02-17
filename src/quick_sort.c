40bb0438cbb552da91178cf076741093b557a438
Dasun <x@dasunsathsara.com>
2024-02-17 10:14:24 +0530

    Implement quick sort and add test cases

--- src/quick_sort.c
+++ src/quick_sort.c
@@ -4,5 +4,46 @@
 
 int quick_sort(int *array, size_t arrLength)
 {
+    int *start = array;
+
+    if (start == NULL)
+    {
+        return -1;
+    }
+
+    // If the array is empty or has only one element, it is already sorted
+    if (arrLength <= 1)
+    {
+        return 0;
+    }
+
+    int pivot;
+    int temp;
+
+    // Set the pivot to the last element in the array
+    pivot = start[arrLength - 1];
+
+    int i = -1;
+    int j = 0;
+
+    // Partition the array
+    while (j < arrLength)
+    {
+        if (start[j] <= pivot)
+        {
+            temp = start[j];
+            i++;
+            start[j] = start[i];
+            start[i] = temp;
+        }
+        j++;
+    }
+
+    size_t left_partition_length = i;
+    size_t right_partition_length = arrLength - i - 1;
+
+    // Recursively sort the two partitions
+    quick_sort(start, left_partition_length);
+    quick_sort(start + i + 1, right_partition_length);
     return 0;
 }
--- tests/test_quick_sort.c
+++ tests/test_quick_sort.c
@@ -1,2 +1,49 @@
 #include "../src/quick_sort.h"
+#include <criterion/criterion.h>
 #include <stdio.h>
+
+Test(quick_sort, test_quick_sort, .description = "Test quick sort")
+{
+    int arr[] = {3, 2, 1, 5, 4};
+    int expected[] = {1, 2, 3, 4, 5};
+    quick_sort(arr, 5);
+    for (int i = 0; i < 5; i++)
+    {
+        cr_assert_eq(arr[i], expected[i]);
+    }
+}
+
+Test(quick_sort, test_quick_sort_empty, .description = "Test quick sort with empty array")
+{
+    int arr[] = {};
+    int r = quick_sort(arr, 0);
+    cr_assert_eq(r, 0);
+}
+
+Test(quick_sort, test_quick_sort_single, .description = "Test quick sort with single element")
+{
+    int arr[] = {1};
+    int r = quick_sort(arr, 1);
+    cr_assert_eq(r, 0);
+}
+
+Test(quick_sort, test_quick_sort_null, .description = "Test quick sort with null array")
+{
+    int *arr = NULL;
+    int r = quick_sort(arr, 0);
+    cr_assert_eq(r, -1);
+}
+
+Test(quick_sort, test_quick_sort_large, .description = "Test quick sort with large array")
+{
+    int arr[1000];
+    for (int i = 0; i < 1000; i++)
+    {
+        arr[i] = 1000 - i;
+    }
+    quick_sort(arr, 1000);
+    for (int i = 0; i < 1000; i++)
+    {
+        cr_assert_eq(arr[i], i + 1);
+    }
+}
