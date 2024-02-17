5026f0fd0d1b3b1d9e6bb4941d298b77945cbc96
Dasun <x@dasunsathsara.com>
2024-02-17 18:14:50 +0530

    Remove print_integer_array function from helpers.c and helpers.h

--- src/helpers.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-/**
- * Prints the elements of an integer array.
- *
- * @param arr The integer array to be printed.
- * @param length The length of the array.
- */
-void print_integer_array(int *arr, size_t length);
